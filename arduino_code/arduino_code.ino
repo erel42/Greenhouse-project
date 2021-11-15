ff474f340878cce68791ffe63122964b80f0d332
erel42 <Erelafu@gmail.com>
2021-11-15 12:12:24 +0200

    Update arduino_code.ino
    Abstaction

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,10 +1,15 @@
+#define NUM_OF_VALUES 1
 int i;
-String input;
-bool exit_switch;
+String input, data;
+bool exit_switch, requestNewRecord = false;
+
+double valueArr[NUM_OF_VALUES];
 
 void LEDon();
 void LEDoff();
-
+void checkInput();
+void updateValues();
+void sendValues();
 void setup()
  
 {
@@ -20,6 +25,31 @@ Serial.println("Input 1 to Turn LED on and 2 to off");
 }
 
 void loop() {
+  checkInput();
+  updateValues();
+  if (requestNewRecord)
+    sendValues();
+}
+
+void updateValues()
+{
+  
+}
+
+void sendValues()
+{
+  data = "";
+  data += valueArr[0];
+  for(i = 1; i < NUM_OF_VALUES; i++)
+  {
+    data += ", ";
+    data += valueArr[i];
+  }
+  Serial.println(data);
+}
+
+void checkInput()
+{
   if (Serial.available())
   {
     input = Serial.readString();
@@ -28,19 +58,24 @@ void loop() {
     exit_switch = false;
     while(!exit_switch && i < input.length())
     {
+      exit_switch = true;
       switch(input[i])
       {
         case ('1'): //LED on
           LEDon();
-          exit_switch = true;
           break;
         case ('2'): //LED off
           LEDoff();
-          exit_switch = true;
           break;
+        case('r'):
+          requestNewRecord = true;
+          break;
+        default:
+          exit_switch = false;
       }
       i++;
     }
+    
   }
 }
 
